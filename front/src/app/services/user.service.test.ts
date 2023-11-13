3bb0011d06c6613e8659fb6738bb531448fd05e0
Léo <75133886+SamouraiDuWeb@users.noreply.github.com>
2023-11-13 17:27:50 +0100

    front services test

---
+++ front/src/app/services/user.service.test.ts
@@ -0,0 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { expect } from '@jest/globals';
+
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[
+        HttpClientModule
+      ]
+    });
+    service = TestBed.inject(UserService);
+  });
+
+  it('Created', () => {
+    expect(service).toBeTruthy();
+  });
+});
